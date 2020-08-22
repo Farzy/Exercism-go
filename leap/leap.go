efa3b19979cb612d1406bdbcbd1bee7ea1004407
Farzad FARID <farzy@farzy.org>
2020-08-22 23:31:12 +0200

    Typo

--- leap/leap.go
+++ leap/leap.go
@@ -2,7 +2,7 @@
 
 package leap
 
-// IsLeapYear return true for a leap year, false otherwise
+// IsLeapYear returns true for a leap year, false otherwise
 func IsLeapYear(year int) bool {
 	return (year%4 == 0) && ((year%100 != 0) || (year%400 == 0))
 }
