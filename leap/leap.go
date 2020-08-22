47ada7f90a73a926c63277b55dffffb32220e347
Farzad FARID <farzy@farzy.org>
2020-08-22 23:30:14 +0200

    Solve leap

---
+++ leap/leap.go
@@ -0,0 +1,8 @@
+// Determine if a year if a leap year
+
+package leap
+
+// IsLeapYear return true for a leap year, false otherwise
+func IsLeapYear(year int) bool {
+	return (year%4 == 0) && ((year%100 != 0) || (year%400 == 0))
+}
