ad9332fe24095c364a871d4178bba0e82773f6e1
Farzad FARID <farzy@farzy.org>
2020-08-25 20:33:31 +0200

    Optimise triangle

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -18,22 +18,8 @@ func KindFromSides(a, b, c float64) Kind {
 	// Exclude Nan, Inf, null and negative numbers
 	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) ||
 		math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) ||
-		a <= 0 || b <= 0 || c <= 0 {
-		return NaT
-	}
-	// Sort by ascending triangle side
-	if a > b {
-		a, b = b, a
-	}
-	if b > c {
-		b, c = c, b
-		if a > b {
-			a, b = b, a
-		}
-	}
-
-	// Impossible triangle, not even flat
-	if a+b < c {
+		a <= 0 || b <= 0 || c <= 0 ||
+		a+b < c || a+c < b || b+c < a {
 		return NaT
 	}
 
