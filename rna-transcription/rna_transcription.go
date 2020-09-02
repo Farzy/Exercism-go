64ac9f2a1bbad31e8b1931c314f0021e74c3d760
Farzad FARID <farzad.farid@kapten.com>
2020-09-02 18:52:40 +0200

    Compare rna_transcription to other solutions in terms of efficiency

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -23,3 +23,60 @@ func ToRNA(dna string) string {
 
 	return str.String()
 }
+
+// Compare to some other solutions
+
+// https://exercism.io/tracks/go/exercises/rna-transcription/solutions/8932a2ee31024ec4a082095b89ff48fd
+func ToRNA_bwarren2(dna string) (out string) {
+	var RNAMap = map[string]string{"G": "C", "C": "G", "T": "A", "A": "U"}
+	for _, value := range dna {
+		out += RNAMap[string(value)]
+	}
+	return
+}
+
+// https://exercism.io/tracks/go/exercises/rna-transcription/solutions/5fbbe4d003222e82656c3834
+func ToRna_mgood(input string) string {
+	return strings.NewReplacer(
+		"G", "C",
+		"C", "G",
+		"T", "A",
+		"A", "U",
+	).Replace(input)
+}
+
+// https://exercism.io/tracks/go/exercises/rna-transcription/solutions/16da121be28d43e89ee1e8ae881b44e8
+var translate = map[rune]byte{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
+// ToRNA - return DNA compliment
+func ToRNA_jonahaapala(dna string) string {
+	rna := make([]byte, len(dna))
+	for i, c := range dna {
+		rna[i] = translate[c]
+	}
+	return string(rna)
+}
+
+// https://exercism.io/tracks/go/exercises/rna-transcription/solutions/ec2dc55237cd4931840020b6cca847a8
+// ToRNA converts a DNA string into an RNA string
+// BenchmarkRNATranscription-4      5000000               301 ns/op
+func ToRNA_kgibilterra(dna string) string {
+	return strings.Map(func(r rune) rune {
+		switch r {
+		case 'G':
+			return 'C'
+		case 'C':
+			return 'G'
+		case 'T':
+			return 'A'
+		case 'A':
+			return 'U'
+		}
+		return r
+	}, dna)
+}
--- rna-transcription/rna_transcription_test.go
+++ rna-transcription/rna_transcription_test.go
@@ -19,3 +19,61 @@ func BenchmarkRNATranscription(b *testing.B) {
 		}
 	}
 }
+
+// Compare to some other solutions
+//
+// === RUN   TestRNATranscription
+//    rna_transcription_test.go:11: PASS: Empty RNA sequence
+//    rna_transcription_test.go:11: PASS: RNA complement of cytosine is guanine
+//    rna_transcription_test.go:11: PASS: RNA complement of guanine is cytosine
+//    rna_transcription_test.go:11: PASS: RNA complement of thymine is adenine
+//    rna_transcription_test.go:11: PASS: RNA complement of adenine is uracil
+//    rna_transcription_test.go:11: PASS: RNA complement
+//--- PASS: TestRNATranscription (0.00s)
+//goos: darwin
+//goarch: amd64
+//pkg: strand
+//BenchmarkRNATranscription
+//BenchmarkRNATranscription-16                	 8941611	       131 ns/op	      16 B/op	       5 allocs/op
+//BenchmarkRNATranscription_bwarren2
+//BenchmarkRNATranscription_bwarren2-16       	  795481	      1280 ns/op	      96 B/op	      11 allocs/op
+//BenchmarkRNATranscription_mgood
+//BenchmarkRNATranscription_mgood-16          	  518358	      2050 ns/op	    2752 B/op	      24 allocs/op
+//BenchmarkRNATranscription_jonahaapala
+//BenchmarkRNATranscription_jonahaapala-16    	 6086840	       196 ns/op	      32 B/op	       6 allocs/op
+//BenchmarkRNATranscription_kgibilterra
+//BenchmarkRNATranscription_kgibilterra-16    	 5887526	       216 ns/op	      37 B/op	       5 allocs/op
+//PASS
+//ok  	strand	6.365s
+
+func BenchmarkRNATranscription_bwarren2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, test := range rnaTests {
+			ToRNA_bwarren2(test.input)
+		}
+	}
+}
+
+func BenchmarkRNATranscription_mgood(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, test := range rnaTests {
+			ToRna_mgood(test.input)
+		}
+	}
+}
+
+func BenchmarkRNATranscription_jonahaapala(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, test := range rnaTests {
+			ToRNA_jonahaapala(test.input)
+		}
+	}
+}
+
+func BenchmarkRNATranscription_kgibilterra(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, test := range rnaTests {
+			ToRNA_kgibilterra(test.input)
+		}
+	}
+}
