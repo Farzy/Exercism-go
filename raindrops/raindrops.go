f403873cc679a41d5827ac9587d86e92736a8862
Farzad FARID <farzad.farid@kapten.com>
2020-09-04 19:05:42 +0200

    Solve raindrops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,1 +1,24 @@
 package raindrops
+
+import "strconv"
+
+// Convert converts a number into a string that contains raindrop sounds
+//
+// Note: A version using strings.Builder is about 33% slower and uses 3 times more
+// memory allocations.
+func Convert(num int) (result string) {
+	if num%3 == 0 {
+		result += "Pling"
+	}
+	if num%5 == 0 {
+		result += "Plang"
+	}
+	if num%7 == 0 {
+		result += "Plong"
+	}
+	if len(result) == 0 {
+		result = strconv.Itoa(num)
+	}
+
+	return
+}
