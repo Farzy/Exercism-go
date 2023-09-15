8ddc2a95c6d00541273d56e937f98fafbcdf47f0
Farzad FARID <farzy@farzy.org>
2023-09-15 22:24:27 +0200

    Update all solved exercises

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -3,7 +3,7 @@ package hamming
 
 import "fmt"
 
-// Distance returns the Hamming distance between 2 DNA strands
+// Distance returns the Hamming distance betweek 2 DNA strands
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return 0, fmt.Errorf("string a (%d) & b (%d) are not of equal length", len(a), len(b))
