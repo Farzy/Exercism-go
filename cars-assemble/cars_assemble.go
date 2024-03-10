c6d6b24ba00a0aafd2b0e40209e224c462363ea1
Farzad FARID <farzy@farzy.org>
2024-03-10 21:15:53 +0100

    Solve updated Cars Assemble

--- cars-assemble/cars_assemble.go
+++ cars-assemble/cars_assemble.go
@@ -1,38 +1,18 @@
 package cars
 
-// SuccessRate is used to calculate the ratio of an item being created without
-// error for a given speed
-func SuccessRate(speed int) float64 {
-	if speed == 0 {
-		return 0.0
-	} else if 1 <= speed && speed <= 4 {
-		return 1.0
-	} else if 5 <= speed && speed <= 8 {
-		return 0.9
-	} else if 9 <= speed && speed <= 10 {
-		return 0.77
-	}
-	panic("Invalide speed value")
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate / 100.0
 }
 
-// CalculateProductionRatePerHour for the assembly line, taking into account
-// its success rate
-func CalculateProductionRatePerHour(speed int) float64 {
-	return float64(speed) * 221.0 * SuccessRate(speed)
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(float64(productionRate) * successRate / 100.0 / 60.0)
 }
 
-// CalculateProductionRatePerMinute describes how many working items are
-// produced by the assembly line every minute
-func CalculateProductionRatePerMinute(speed int) int {
-	return int(CalculateProductionRatePerHour(speed) / 60)
-}
-
-// CalculateLimitedProductionRatePerHour describes how many working items are
-// produced per hour with an upper limit on how many can be produced per hour
-func CalculateLimitedProductionRatePerHour(speed int, limit float64) float64 {
-	rate := CalculateProductionRatePerHour(speed)
-	if rate > limit {
-		rate = limit
-	}
-	return rate
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	return uint((carsCount/10)*95000 + (carsCount%10)*10000)
 }
