c2582eedbbdf4e4067817bd3dce38ae42a96fc22
Farzad FARID <farzy@farzy.org>
2024-03-11 19:43:54 +0100

    Improve last function

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -51,8 +51,5 @@ func RemoveItem(bill, units map[string]int, item, unit string) bool {
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
 	currentAmount, exists := bill[item]
-	if !exists {
-		return 0, false
-	}
-	return currentAmount, true
+	return currentAmount, exists
 }
