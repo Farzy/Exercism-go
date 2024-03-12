46b35a08db55a404116f5b0adc2638364bac0079
Farzad FARID <farzy@farzy.org>
2024-03-12 18:39:57 +0100

    Do some benchmarks on Luhn

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,8 +1,52 @@
 package luhn
 
-import "unicode"
+/*
+goos: darwin
+goarch: amd64
+pkg: luhn
+cpu: Intel(R) Core(TM) i7-8850H CPU @ 2.60GHz
+BenchmarkValid
+BenchmarkValid-12            	 3870295	       291.9 ns/op	       0 B/op	       0 allocs/op
+BenchmarkValidRune
+BenchmarkValidRune-12        	 1553277	       766.0 ns/op	     176 B/op	       1 allocs/op
+BenchmarkValidExercism
+BenchmarkValidExercism-12    	  501820	      2196 ns/op	    1024 B/op	      36 allocs/op
+*/
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
 
 func Valid(id string) bool {
+	var digitCount, sum int
+	var doubleDigit bool
+
+	for i := len(id) - 1; i >= 0; i-- {
+		c := id[i]
+		if c == ' ' {
+			continue
+		}
+		digit := int(c - '0')
+		if digit >= 0 && digit <= 9 {
+			if doubleDigit {
+				digit *= 2
+				if digit > 9 {
+					digit -= 9
+				}
+			}
+			sum += digit
+			digitCount++
+			doubleDigit = !doubleDigit
+		} else {
+			return false
+		}
+	}
+	return digitCount > 1 && (sum%10) == 0
+}
+
+func ValidRune(id string) bool {
 	digitCount := 0
 	sum := 0
 	doubleDigit := false
@@ -29,4 +73,32 @@ func Valid(id string) bool {
 		return false
 	}
 	return true
+}
+
+// This example code from https://exercism.org/tracks/go/exercises/luhn/dig_deeper does
+// not even pass the long number test because of an integer overflow!
+func ValidExercism(num string) bool {
+	num = strings.ReplaceAll(num, " ", "")
+	if _, err := strconv.Atoi(num); err != nil {
+		return false
+	}
+	total := 0
+	pos := 0
+	for i := len(num) - 1; i > -1; i-- {
+		digit := int(num[i] - '0')
+		if pos%2 == 0 {
+			total += digit
+		} else {
+			switch digit {
+			case 1, 2, 3, 4:
+				total += digit << 1
+			case 5, 6, 7, 8:
+				total += (digit << 1) - 9
+			case 9:
+				total += digit
+			}
+		}
+		pos++
+	}
+	return pos > 1 && total%10 == 0
 }
--- luhn/luhn_test.go
+++ luhn/luhn_test.go
@@ -22,3 +22,45 @@ func BenchmarkValid(b *testing.B) {
 		}
 	}
 }
+
+func TestValidRune(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			if actual := ValidRune(tc.input); actual != tc.expected {
+				t.Fatalf("ValidRune(%q) = %t, want: %t", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkValidRune(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping benchmark in short mode.")
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testCases {
+			ValidRune(tc.input)
+		}
+	}
+}
+
+//func TestValidExercism(t *testing.T) {
+//	for _, tc := range testCases {
+//		t.Run(tc.description, func(t *testing.T) {
+//			if actual := ValidExercism(tc.input); actual != tc.expected {
+//				t.Fatalf("ValidExercism(%q) = %t, want: %t", tc.input, actual, tc.expected)
+//			}
+//		})
+//	}
+//}
+
+func BenchmarkValidExercism(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping benchmark in short mode.")
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testCases {
+			ValidExercism(tc.input)
+		}
+	}
+}
