45b119bf3f6a8088583f3d5b7d392d56a4319a7d
Farzad FARID <farzy@farzy.org>
2024-03-12 17:48:22 +0100

    Solve and benchmark Difference of Squares

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,33 @@
+package diffsquares
+
+func SquareOfSum(n int) int {
+	if n < 0 {
+		panic("n must be a positive number")
+	}
+	return n * n * (n + 1) * (n + 1) / 4
+}
+
+func SumOfSquares(n int) int {
+	if n < 0 {
+		panic("n must be a positive number")
+	}
+	return n * (n + 1) * (2*n + 1) / 6
+}
+
+func SumOfSquaresSimple(n int) (sum int) {
+	if n < 0 {
+		panic("n must be a positive number")
+	}
+	for i := range n {
+		sum += (i + 1) * (i + 1)
+	}
+	return
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
+
+func DifferenceSimple(n int) int {
+	return SquareOfSum(n) - SumOfSquaresSimple(n)
+}
