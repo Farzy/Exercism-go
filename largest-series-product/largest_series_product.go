8759ae7f7547b130cf39e88cb2f04abd2ce89f86
Farzad FARID <farzy@farzy.org>
2024-03-14 22:42:46 +0100

    Solve Largest Series Product with the involuntary help of AI

--- largest-series-product/largest_series_product.go
+++ largest-series-product/largest_series_product.go
@@ -1,5 +1,29 @@
 package lsproduct
 
+import (
+	"fmt"
+	"unicode"
+)
+
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	numDigits := len(digits)
+	if span < 1 || span > numDigits {
+		return 0, fmt.Errorf("span of %d is larger than the digits string (%d)", span, numDigits)
+	}
+	var maxProduct int64
+	for i := 0; i <= numDigits-span; i++ {
+		product := int64(1)
+		for j := i; j < i+span; j++ {
+			digitRune := rune(digits[j])
+			if !unicode.IsNumber(digitRune) {
+				return 0, fmt.Errorf("found non digit in string: '%c'", digitRune)
+			}
+			digit := int64(digitRune - '0')
+			product *= digit
+		}
+		if product > maxProduct {
+			maxProduct = product
+		}
+	}
+	return maxProduct, nil
 }
