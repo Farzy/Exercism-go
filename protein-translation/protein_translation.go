e2e2f508bac997b0a52c4626059a9332a89170f6
Farzad FARID <farzy@farzy.org>
2024-03-14 16:45:13 +0100

    Implement protein translation from RNA sequences
    The code now supports translation of RNA sequences into proteins by implementing 'FromRNA' and 'FromCodon' functions. Additionally, two new error types have been added to handle stop codons and invalid bases. A map linking codons to proteins has been created for correct translation.

--- protein-translation/protein_translation.go
+++ protein-translation/protein_translation.go
@@ -1,9 +1,57 @@
 package protein
 
+import "errors"
+
+var ErrStop = errors.New("stop codon detected")
+var ErrInvalidBase = errors.New("invalid base")
+
+var codonToProtein = map[string]string{
+	"AUG": "Methionine",
+	"UUU": "Phenylalanine",
+	"UUC": "Phenylalanine",
+	"UUA": "Leucine",
+	"UUG": "Leucine",
+	"UCU": "Serine",
+	"UCC": "Serine",
+	"UCA": "Serine",
+	"UCG": "Serine",
+	"UAU": "Tyrosine",
+	"UAC": "Tyrosine",
+	"UGU": "Cysteine",
+	"UGC": "Cysteine",
+	"UGG": "Tryptophan",
+	"UAA": "STOP",
+	"UAG": "STOP",
+	"UGA": "STOP",
+}
+
 func FromRNA(rna string) ([]string, error) {
-	panic("Please implement the FromRNA function")
+	if len(rna)%3 != 0 {
+		return nil, ErrInvalidBase
+	}
+
+	var proteins []string
+
+	for i := 0; i < len(rna); i += 3 {
+		protein, err := FromCodon(rna[i : i+3])
+		if err == ErrInvalidBase {
+			return nil, ErrInvalidBase
+		}
+		if err == ErrStop {
+			return proteins, nil
+		}
+		proteins = append(proteins, protein)
+	}
+	return proteins, nil
 }
 
 func FromCodon(codon string) (string, error) {
-	panic("Please implement the FromCodon function")
+	protein, ok := codonToProtein[codon]
+	if !ok {
+		return "", ErrInvalidBase
+	}
+	if protein == "STOP" {
+		return "", ErrStop
+	}
+	return protein, nil
 }
