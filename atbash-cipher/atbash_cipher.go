319b699fd1f5507a9f47aa9731f95d4670dd5a20
Farzad FARID <farzy@farzy.org>
2024-03-15 19:19:07 +0100

    Solve Atbash Cipher

--- atbash-cipher/atbash_cipher.go
+++ atbash-cipher/atbash_cipher.go
@@ -1,5 +1,26 @@
 package atbash
 
+import (
+	"strings"
+	"unicode"
+)
+
+const atbashAlphabet = "zyxwvutsrqponmlkjihgfedcba"
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	output := strings.Builder{}
+	for _, chr := range s {
+		if unicode.IsNumber(chr) {
+			output.WriteRune(chr)
+		} else if !unicode.IsLetter(chr) {
+			continue
+		} else {
+			output.WriteByte(atbashAlphabet[unicode.ToLower(chr)-'a'])
+		}
+		// Add a space every 5 characters, use a hack to account for the spaces we add.
+		if (output.Len()+1)%6 == 0 {
+			output.WriteByte(' ')
+		}
+	}
+	return strings.TrimSpace(output.String())
 }
